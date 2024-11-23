a62477ac1165f8c92a787ee095a528afa2a96ff4
Chad <71134463+ch444d@users.noreply.github.com>
2024-11-22 20:14:37 -0500

    cleaned up input

--- 27-guessgame.py
+++ 27-guessgame.py
@@ -9,17 +9,17 @@ guesses = 0
 # print(low)
 # print(high)
 
-player_guess = int(input(f"Enter a guess between {low} and {high}: (whole number)"))
+player_guess = int(input(f"Enter a guess between {low} and {high} (whole number): "))
 
 while player_guess != number:
     if player_guess > number:
         print("Too high, guess again.")
         guesses += 1
-        player_guess = int(input(f"Enter a guess between {low} and {high}: (whole number)"))
+        player_guess = int(input(f"Enter a guess between {low} and {high} (whole number): "))
     if player_guess < number:
         print("Too low, guess again.")
         guesses += 1
-        player_guess = int(input(f"Enter a guess between {low} and {high}: (whole number)"))
+        player_guess = int(input(f"Enter a guess between {low} and {high} (whole number): "))
 
 if player_guess == number:
     print("Congrats, you win!")
